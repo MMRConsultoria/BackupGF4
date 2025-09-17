533fec6f158cc4945d2f0114b48bdbf5d411b9d6
MMR Consultoria <maricelisrossi@gmail.com>
2025-09-17 16:36:20 -0300

    Update Login.py

--- pages/Login.py
+++ pages/Login.py
@@ -6,7 +6,7 @@ import json
 from datetime import datetime
 import streamlit as st
 
-st.set_page_config(page_title="Login | MMR Consultoria")
+#st.set_page_config(page_title="Login | MMR Consultoria")
 
 # =====================================
 # CSS para esconder barra de botões do canto superior direito
@@ -119,7 +119,7 @@ def registrar_acesso(nome_usuario):
 
 # ✅ Redireciona se já estiver logado
 if st.session_state.get("acesso_liberado"):
-    st.switch_page("Home.py")
+    st.rerun()
 
 # ✅ Exibe o IP do usuário discretamente
 #st.markdown(f"<p style='font-size:12px; color:#aaa;'>🛠️ Seu IP: <code>{ip_usuario}</code></p>", unsafe_allow_html=True)
@@ -144,7 +144,7 @@ if st.button("Entrar"):
         st.session_state["empresa"] = codigo
         st.session_state["usuario_logado"] = email
         registrar_acesso(email)
-        st.switch_page("Home.py")
+        st.rerun()
 
     else:
         st.error("❌ Código, e-mail ou senha incorretos.")

00420c345404c7f69994c634418f87d923c8b606
MMR Consultoria <maricelisrossi@gmail.com>
2025-11-18 16:16:18 -0300

    Delete pages/texte.py

--- pages/texte.py
+++
@@ -1,6 +0,0 @@
-import json, streamlit as st
-from oauth2client.service_account import ServiceAccountCredentials
-
-secret = st.secrets.get("GOOGLE_SERVICE_ACCOUNT")
-credentials_dict = json.loads(secret) if isinstance(secret, str) else dict(secret)
-st.write("Service account:", credentials_dict.get("client_email"))

ab11cc29074858b4b3cac99b62b2d8fcd6701fa6
MMR Consultoria <maricelisrossi@gmail.com>
2025-12-31 09:00:27 -0300

    Update db_utils.py

--- pages/db_utils.py
+++ pages/db_utils.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import psycopg2
+
+CERT_PATH = "aws-us-east-2-bundle.pem"
+
+# Grava o certificado em arquivo só uma vez por sessão
+if "cert_written" not in st.session_state:
+    with open(CERT_PATH, "w") as f:
+        f.write(st.secrets["certs"]["aws_rds_us_east_2"])
+    st.session_state["cert_written"] = True
+
+def get_conn():
+    conn = psycopg2.connect(
+        host=st.secrets["db"]["host"],
+        port=st.secrets["db"]["port"],
+        dbname=st.secrets["db"]["database"],
+        user=st.secrets["db"]["user"],
+        password=st.secrets["db"]["password"],
+        sslmode="verify-full",
+        sslrootcert=CERT_PATH,
+    )
+    return conn
 

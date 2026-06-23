20871d4bae97f255cc5e1856aa79bf82da469c3c
MMR Consultoria <maricelisrossi@gmail.com>
2026-06-23 14:30:37 -0300

    Update Teste_Zig_API.py

--- pages/Teste_Zig_API.py
+++ pages/Teste_Zig_API.py
@@ -1,1 +1,10 @@
+url = "https://api.zigcore.com.br/integration/erp/lojas"
 
+headers = {
+    "Authorization": token
+}
+
+resp = requests.get(url, headers=headers)
+
+print(resp.status_code)
+print(resp.text)
